070acb2975978205e843e1c45ddf9fc5596686f8
Fribee22 <antoni.fribe22@gmail.com>
2025-10-27 17:03:43 +0100

    Add files via upload

---
+++ zad 7 lab2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+	float liczba;
+	
+
+	for (int i = 0; i < 10; i++) {
+		cout << "podaj liczbe rzecz";
+		cin >> liczba;
+
+		if (liczba > ) {
+			cout << "dodatnia liczba" << liczba << endl;
+		}
+
+	}
+		
+
+	return 0;
+
+}
