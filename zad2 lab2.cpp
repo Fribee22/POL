40f3b35da376e4f4947c45963287e11d516747c9
Fribee22 <antoni.fribe22@gmail.com>
2025-10-27 16:39:49 +0100

    lab 2 poprawne

--- zad2 lab2.cpp
+++ zad2 lab2.cpp
@@ -1,3 +1,17 @@
+//////////////zad1
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 10; i >= 0; i--) {
+        cout << i << endl;
+    }
+    return 0;
+}
+
+////////////////////////////zad2
+
+
+
 #include <stdio.h>
 #include <iostream>
 using namespace std;
@@ -20,3 +34,73 @@ int main() {
 
 
 }
+/////////////////////////////zad3
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+    int fahr, celsius;
+    int start, limit, krok;
+
+    start = 0;
+    limit = 200;
+    krok = 20;
+
+    for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)
+    {
+        float celsius = (5.0 / 9.0) * (fahr - 32.0);
+        cout << fahr << "\t" << celsius << endl;
+    }
+
+
+
+
+}
+//////////////////zad5, liczba MUSI byæ z przecinek 
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+    int fahr, celsius;
+    int start, limit, krok;
+
+    start = 0;
+    limit = 200;
+    krok = 20;
+
+    for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)
+    {
+        float celsius = (5 / 9) * (fahr - 32.0);
+        cout << fahr << "\t" << celsius << endl;
+    }
+
+
+
+
+}
+/////////////////zad6
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+    int fahr, celsius;
+    int start, limit, krok;
+
+    start = 0;
+    cin >> limit;
+    cin >> krok;
+
+    for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)
+    {
+        int yest = 0;
+        float celsius = (5.0 / 9.0) * (fahr - 32.0);
+        cout << fahr << "\t" << celsius << endl;
+    }
+
+
+
+
+}
+////////////zad7
