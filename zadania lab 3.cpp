0c5002a1298d88d21b0f3b33a4eee2bcb5062127
Fribee22 <antoni.fribe22@gmail.com>
2025-10-27 18:15:43 +0100

    Add files via upload

---
+++ zadania lab 3.cpp
@@ -0,0 +1,163 @@
+///////////////zad1
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+    
+    int a, b;
+    
+    cin >> a;
+    cin >> b;
+    
+    if (a == b){
+        cout << "liczby są sobie równe" << endl;
+    }
+    else if (a > b){
+        cout << "liczba a jest wieksza od b" << endl;
+    }
+    else{
+        cout << "liczba b jest wieksza od a" << endl;
+    }
+    
+    
+        
+            
+        
+    
+    
+}
+
+//////////zad2
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+    
+    int a, b;
+    
+    cin >> a;
+    cin >> b;
+    
+    if (a == b){
+        cout << "liczby są sobie równe " << endl;
+    }
+    else{
+        if (a > b){
+            cout << "liczba a jest wieksza od b" << endl;
+            }else {
+                cout << "liczba b jest wieksza od a" << endl;
+            }
+        
+    }
+      
+    
+    
+        
+            
+        
+    
+    
+}
+//////////////////////zad3
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+    
+    float r;
+    
+    cin >> r;
+    
+    if (0.7 <= r <= 1){
+        cout << "bardzo silna korelacja" << endl;
+    }
+    else if(r>=0.5){
+        cout << "silna korelacja dodatnia" << endl;
+        
+    }
+    else if(r>=0.3){
+        cout << "umiarkowana korelacja" << endl;
+    }
+    else if(r>=0.2){
+        cout << "słaba korelacja dodatnia" << endl;
+    }
+    else if(r>=0){
+        cout << "brak  korelacja" << endl;
+    }
+    else{
+        cout << "wtf" << endl;
+    }
+    
+    
+        
+            
+        
+    
+    
+}
+///////////////zad4
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main (){
+    
+    int opcja;
+    int a, b;
+    
+    cout << "wpisz z klawiatury 1 - dodawanie, 2 - odejmwoanie, 3 - mnożenie, 4 - dzielnie: ";
+    cin >> opcja;
+    
+    cout << "Podaj liczbe a: ";
+    cin >> a;
+    cout << "Podaj liczbe b: ";
+    cin >> b;
+    
+    switch (opcja){
+        case 1:
+        cout << "Suma: " << a + b << endl;
+        break;
+        case 2:
+        cout << "Różnica: " << a - b << endl;
+        break;
+        case 3:
+        cout << "iloczyn: " << a * b << endl;
+        break;
+        case 4:
+        if (b == 0){
+            cout << "nie dziel przez zero" << endl;
+        }
+        else{
+            cout << "iloraz: " << a / b << endl;
+        
+        
+        }
+        break;
+        
+        
+        
+        
+        
+        
+        
+    }
+///////////zad 5
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 0; ; i + 2){
+        cout << i;
+    }
+    return 0;
+}
+
+///////////zad6
+
