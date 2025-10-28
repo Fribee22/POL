1bfe76bb2c76d79ddad3024ff0e89cced87c9526
student <student@DESKTOP-5POI41D>
2025-10-28 14:45:13 +0100

    lab 4

---
+++ Zadania lab 4/Zadania lab 4/Zadania lab 4.cpp
@@ -0,0 +1,62 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+void fir(int a, int b);
+int sec(int a, int b);
+
+int main() {
+	int a;
+	int b;
+
+	cout << "podaj pierwsza liczbe: " << endl;
+	cin >> a;
+	cout << "podaj druga liczbe: " << endl;
+	cin >> b;
+
+	fir(a, b);
+	sec(a, b);
+}
+
+
+
+
+void fir(int a, int b) {
+	if (a > b) {
+		cout << a << "jest wieksze od" << b << endl;
+
+	}
+	else if (b > a) {
+		cout << b << " jest wieksze od " << a << endl;
+
+	}
+	else {
+		cout << "liczby są równe" << endl;
+	}
+
+
+}
+int sec(int a, int b) {
+	if (a > b) {
+		cout << a << endl;
+	}
+	else {
+		cout << b;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
