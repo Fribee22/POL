1954587ade248d82559cdd59f9d17e292c0c3e99
Fribee22 <antoni.fribe22@gmail.com>
2025-11-04 15:01:41 +0100

    Add files via upload

---
+++ lab 5.cpp
@@ -0,0 +1,63 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+/*
+int main() {
+	int tab[10];
+
+	for (int i = 0; i < 10; i++) {
+		cout << "wprowadz liczbe" << endl;
+		cin >> tab[i];
+
+	}
+
+	for (int i = 9; i >= 0; i--) {
+		cout << tab[i] << endl;
+	}
+
+
+	
+
+}
+*/
+
+int main() {
+
+	int liczby;
+	
+
+	
+	cout << "ile liczb chcesz?" << endl;
+
+	cin >> liczby;
+	int* tab = new int[liczby];
+
+	tab[0] =  0;
+	tab[1] =  1;
+
+	for (int i = 2; i <= liczby; i++) {
+		tab[i] = tab[i - 1] + tab[i - 2];
+		cout << liczby << endl;
+
+
+	}
+
+
+
+
+
+
+
+	
+
+
+
+
+
+	
+	
+}
+
