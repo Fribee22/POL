4db8e2197f02ae36848be7102c16d244b36e3960
Fribee22 <antoni.fribe22@gmail.com>
2025-11-18 15:01:47 +0100

    lab 6

---
+++ lab 6.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdio.h>
+
+
+using namespace std;
+/*
+int a = 20;
+int b = 10;
+
+
+
+
+
+
+int suma(){
+    return a + b;
+
+    
+}
+
+int main(){
+    int a = 1;
+    int b = 2;
+
+    cout << suma << endl;
+}
+*/
+
+int tab[10];
+int rozmiar = 10;
+
+int usun(int tab[10]) {
+    int przesun;
+    cout << "podaj liczbe od 0 do 9" << endl;
+    cin >> przesun;
+    
+
+
+}
+
+
+
+
+int main() {
+    cout << "podaj 10 liczb calkowitych" << endl;
+    for (int i = 0; i < 9; i++) {
+        cin >> tab[9];
+        return usun;
+    }
+    cout << tab[10] << endl;
+}
+
+
+
+
+
