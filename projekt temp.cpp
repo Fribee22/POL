5f4ec252d1c3c99b310b035bd8ebece7b1aed1e0
Fribee22 <antoni.fribe22@gmail.com>
2026-01-04 14:03:31 +0100

    Update projekt temp.cpp

--- projekt temp.cpp
+++ projekt temp.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 
 
+
+double historiaWartosci[100] = { 0.0 };
+
+
+char historiaJednostek[100] = { 0 };
+
+
+
+int dataCounter = 0;
+
+
+
 float FtoC(float stopnie);
 float FtoK(float stopnie);
 float CtoF(float stopnie);
@@ -12,29 +24,14 @@ float CtoK(float stopnie);
 float KtoC(float stopnie);
 float KtoF(float stopnie);
 
+float check(float temp, char stopnie);
 
-float check(float temp, char stopnie) {
-    if (stopnie == 'K' && temp < 0) return -999.0;
-    else if (stopnie == 'C' && temp < -273.15) return -999.0;
-    else if (stopnie == 'F' && temp < -459.67) return -999.0;
-    return temp;
-}
-
-
-float CtoF(float stopnie) { return stopnie * (9.0 / 5.0) + 32.0; }
-float CtoK(float stopnie) { return stopnie + 273.15; }
-float FtoC(float stopnie) { return (5.0 / 9.0) * (stopnie - 32.0); }
-float FtoK(float stopnie) { return (stopnie + 459.67) * (5.0 / 9.0); }
-float KtoC(float stopnie) { return stopnie - 273.15; }
-float KtoF(float stopnie) { return stopnie * (9.0 / 5.0) - 459.67; }
 
 int main() {
     float stopnie;
     int wybor;
 
-    
     while (true) {
-        
         system("cls"); 
 
         cout << "=== MENU KONWERSJI TEMPERATUR ===" << endl;
@@ -45,57 +42,112 @@ int main() {
         cout << "5. przelicz Kelwin na Fahr \n";
         cout << "6. przelicz kelwin na Celsius \n";
         cout << "7. KONIEC PROGRAMU \n";
+        cout << "8. Pokaz historie \n"; 
         cout << "=================================" << endl;
 
         cout << "wybierz numer: ";
         cin >> wybor;
 
-       
+        
         if (wybor == 7) {
-            return 0; 
+            return 0;
         }
 
         
-        if (wybor < 1 || wybor > 7) {
+        if (wybor == 8) {
+            cout << "\n=== HISTORIA PRZELICZEN ===" << endl;
+            if (dataCounter == 0) {
+                cout << "(Brak historii)" << endl;
+            }
+            else {
+               
+                for (int i = 0; i < dataCounter / 2; i++) {
+                    
+                    int index = i * 2;
+                    
+                    cout << "<" << i + 1 << "> "
+                         << historiaWartosci[index] << historiaJednostek[index] 
+                         << " = "
+                         << historiaWartosci[index + 1] << historiaJednostek[index + 1] 
+                         << endl;
+                }
+            }
+        }
+       
+        else if (wybor < 1 || wybor > 8) {
             cout << "Nie ma takiej opcji w menu!" << endl;
-        } 
+        }
+        
         else {
-            
             cout << "podaj temperature do przeliczenia: ";
             cin >> stopnie;
 
-            
-            char skalaWejsciowa;
+            char skalaWejsciowa, skalaWynikowa;
+
+           
             if (wybor == 1 || wybor == 2) skalaWejsciowa = 'F';
             else if (wybor == 3 || wybor == 4) skalaWejsciowa = 'C';
             else skalaWejsciowa = 'K';
 
-            
+           
             float wynikCheck = check(stopnie, skalaWejsciowa);
 
             if (wynikCheck == -999.0f) {
-                
-                cout << "\nBLAD: Nie ma takiej temperatury!" << endl;
-                
-            } else {
-                
-                cout << "\nWynik: ";
+                cout << "\nBLAD: Nie ma takiej temperatury (ponizej zera absolutnego)!" << endl;
+            }
+            else {
+                float wynik = 0;
+
                 switch (wybor) {
-                    case 1: cout << stopnie << " F = " << FtoC(stopnie) << " C"; break;
-                    case 2: cout << stopnie << " F = " << FtoK(stopnie) << " K"; break;
-                    case 3: cout << stopnie << " C = " << CtoF(stopnie) << " F"; break;
-                    case 4: cout << stopnie << " C = " << CtoK(stopnie) << " K"; break;
-                    case 5: cout << stopnie << " K = " << KtoF(stopnie) << " F"; break;
-                    case 6: cout << stopnie << " K = " << KtoC(stopnie) << " C"; break;
+                case 1: wynik = FtoC(stopnie); skalaWynikowa = 'C'; break;
+                case 2: wynik = FtoK(stopnie); skalaWynikowa = 'K'; break;
+                case 3: wynik = CtoF(stopnie); skalaWynikowa = 'F'; break;
+                case 4: wynik = CtoK(stopnie); skalaWynikowa = 'K'; break;
+                case 5: wynik = KtoF(stopnie); skalaWynikowa = 'F'; break;
+                case 6: wynik = KtoC(stopnie); skalaWynikowa = 'C'; break;
                 }
-                cout << endl;
+
+                
+                cout << "\nWynik: " << stopnie << " " << skalaWejsciowa
+                     << " = " << wynik << " " << skalaWynikowa << endl;
+
+               
+                if (dataCounter < 98) {
+                    
+                    historiaWartosci[dataCounter] = stopnie;
+                    historiaJednostek[dataCounter] = skalaWejsciowa;
+
+                    
+                    historiaWartosci[dataCounter + 1] = wynik;
+                    historiaJednostek[dataCounter + 1] = skalaWynikowa;
+
+                    
+                    dataCounter += 2;
+                }
+                else {
+                    cout << "[Ostrzezenie] Brak miejsca w historii!" << endl;
+                }
             }
         }
 
         
         cout << "\nNacisnij ENTER, aby wrocic do menu...";
-        cin.ignore(); 
-        cin.get();    
+        cin.ignore();
+        cin.get();
     }
 }
 
+
+float check(float temp, char stopnie) {
+    if (stopnie == 'K' && temp < 0) return -999.0f;
+    else if (stopnie == 'C' && temp < -273.15f) return -999.0f;
+    else if (stopnie == 'F' && temp < -459.67f) return -999.0f;
+    return temp;
+}
+
+float CtoF(float stopnie) { return stopnie * (9.0f / 5.0f) + 32.0f; }
+float CtoK(float stopnie) { return stopnie + 273.15f; }
+float FtoC(float stopnie) { return (5.0f / 9.0f) * (stopnie - 32.0f); }
+float FtoK(float stopnie) { return (stopnie + 459.67f) * (5.0f / 9.0f); }
+float KtoC(float stopnie) { return stopnie - 273.15f; }
+float KtoF(float stopnie) { return stopnie * (9.0f / 5.0f) - 459.67f; }
